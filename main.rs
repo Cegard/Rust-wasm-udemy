a4d0c90c2aad081b9a218db2f5f6a30ca6d05b76
Eduardo Galeano <cegard689@gmail.com>
2022-10-23 13:02:00 +0200

    2) DEclaring variables.

--- main.rs
+++ main.rs
@@ -1,5 +1,16 @@
 
 fn main() {
-    let message = "Hello World";
-    println!("{}", message);
+    let greeting;
+    greeting = "Hello World";
+    println!("{}", greeting);
+
+    let ten = 6;
+    println!("{} is not ten", ten);
+    let ten = 10;
+    println!("Now {ten} is ten after shadowing.");
+
+    let mut bye = "";
+    println!("{bye}");
+    bye = "Bye!";
+    println!("{bye}");
 }
