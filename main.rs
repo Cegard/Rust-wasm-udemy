13d0c0cc85aa8bf33e3af0dbdffc6a81c6db9cc8
Eduardo Galeano <cegard689@gmail.com>
2022-10-24 22:17:33 +0200

    7) Floats, a bit of tuples and arrays.

--- main.rs
+++ main.rs
@@ -1,12 +1,21 @@
 
 fn main() {
-    let custom_num = 95_000_000; // underscores don't affect the number
-    let hex_num = 0xfa; // a hexadecimal number
-    let bin_num = 0b1001010; // a binary number
-    let byte_num = b'R'; // UTF encoding (UTF is mainly writen in hex code)
+    let float_num: f32 = 3.1459; // By default floating numbers are of 64 bits
+    let float_num_2 = 64.0;
+    println!("{float_num} is a f32 number");
+    println!("{float_num_2} is a f64 number");
 
-    println!("let custom_num = 95_000_000; \t => \t {custom_num}");
-    println!("let hex_num = 0xfa; \t\t => \t {hex_num}");
-    println!("let bin_num = 0b1001010; \t => \t {bin_num}");
-    println!("let byte_num = b'R'; \t\t => \t {byte_num}");
+    // tuples
+    let the_tuple: (i8, &str, f32) = (64, "real", 3.1459);
+    println!("positions in tuples are accessed by dot .");
+    println!("{} {} {}", the_tuple.0, the_tuple.1, the_tuple.2);
+
+    let (zero, one, two) = the_tuple; // unfold
+    println!("{zero} {one} {two}");
+
+    let x = [1, 4, 35, 67, 8];
+    println!("arrays are accessed by [] {} (just to test {{}})", x[4]);
+
+    let y = [0; 5]; // another way to declare arrays
+    println!("arrays are accessed by [] y[4] => {} ", y[4]);
 }
