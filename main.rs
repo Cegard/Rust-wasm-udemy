2a9d2311b73ecead816cfe4fbf9ec1b25ea03e5b
Eduardo Galeano <cegard689@gmail.com>
2022-10-24 20:26:13 +0200

    6) Numbers representations.

--- main.rs
+++ main.rs
@@ -1,21 +1,12 @@
 
 fn main() {
-    let is_true: bool = true;
-    println!("{is_true} comes from a bool var.");
+    let custom_num = 95_000_000; // underscores don't affect the number
+    let hex_num = 0xfa; // a hexadecimal number
+    let bin_num = 0b1001010; // a binary number
+    let byte_num = b'R'; // UTF encoding (UTF is mainly writen in hex code)
 
-    let max_i8_val: i8 = 127;
-    let min_i8_val: i8 = -128;
-    println!("{min_i8_val} and {max_i8_val} come from 'i8' number type variables.");
-
-    let bigger: u128 = 100000000000000000000000000000000;
-    println!("{bigger} fits in a u128 number type.");
-
-    let arch_size: usize = 18446744073709551615;
-    let explicit_sixtyfour: u64 = 18446744073709551615;
-    println!(
-        "{} is a 'usize' number type and is the same as an 'u64' like {}",
-        arch_size,
-        explicit_sixtyfour
-    );
-    println!("and that's because of the most CPU's architecture nowadays");
+    println!("let custom_num = 95_000_000; \t => \t {custom_num}");
+    println!("let hex_num = 0xfa; \t\t => \t {hex_num}");
+    println!("let bin_num = 0b1001010; \t => \t {bin_num}");
+    println!("let byte_num = b'R'; \t\t => \t {byte_num}");
 }
