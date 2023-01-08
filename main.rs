fcf6bf015b5dae7fa79452c5696f49e0254a85bb
Eduardo Galeano <cegard689@gmail.com>
2023-01-08 23:25:27 +0100

    Modifying fields.

--- main.rs
+++ main.rs
@@ -14,14 +14,18 @@ impl Person {
         println!("Hello");
     }
 
-    // Method
+    // Methods
     fn greet(&self) {
         println!("Hello I'm {} {}", self.name, self.last_name);
+    }
+
+    fn happy_birthday(&mut self) {
+        self.age += 1;
     }
 }
 
 fn main() {
-    let person = Person {
+    let mut person = Person {
         name: String::from("foo"),
         last_name: "bar".to_string(),
         age: 30
@@ -29,11 +33,17 @@ fn main() {
 
     person.greet();
 
+    println!("\nOld values");
+    print!("{}\t{}\t{}\n", person.name, person.last_name, person.age);
+    person.happy_birthday();
+
     let name = person.name;
     let last_name = person.last_name;
     let age = person.age;
 
-    print!("{name}\n{last_name}\n{age}\n");
+    println!("\nNew values");
+    print!("{name}\t{last_name}\t{age}\n");
+    
     Person::say_hello();
 }
  
