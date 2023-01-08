9c3c8ce9c2a63bd19f77966e0d7898cdbd459a8c
Eduardo Galeano <cegard689@gmail.com>
2023-01-08 21:12:12 +0100

    structs functions and methods.

--- main.rs
+++ main.rs
@@ -1,8 +1,23 @@
 
 struct Person{
+    // fields //
     name: String,
     last_name: String,
     age: u32,
+    ///////////
+}
+
+    
+impl Person {
+    // Associated fuction, function called from the struct, not from the instaced object
+    fn say_hello() {
+        println!("Hello");
+    }
+
+    // Method
+    fn greet(&self) {
+        println!("Hello I'm {} {}", self.name, self.last_name);
+    }
 }
 
 fn main() {
@@ -12,10 +27,13 @@ fn main() {
         age: 30
     };
 
+    person.greet();
+
     let name = person.name;
     let last_name = person.last_name;
     let age = person.age;
 
     print!("{name}\n{last_name}\n{age}\n");
+    Person::say_hello();
 }
  
