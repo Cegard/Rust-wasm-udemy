6ac3e46bdd9536b9f1a6f0a50958fa15e6839686
Eduardo Galeano <cegard689@gmail.com>
2023-01-08 23:42:21 +0100

    Constructor

--- main.rs
+++ main.rs
@@ -9,9 +9,23 @@ struct Person{
 
     
 impl Person {
+
     // Associated fuction, function called from the struct, not from the instaced object
     fn say_hello() {
         println!("Hello");
+    }
+
+    // Empty constructor
+    fn new_empty() -> Person {
+        return Person { name: "_".to_string(), last_name: String::from("_"), age: 1 };
+    }
+
+    fn new(name: String, last_name: String, age: u32) -> Person {
+        return Person{
+            name,
+            last_name,
+            age
+        };
     }
 
     // Methods
@@ -25,16 +39,20 @@ impl Person {
 }
 
 fn main() {
-    let mut person = Person {
-        name: String::from("foo"),
-        last_name: "bar".to_string(),
-        age: 30
-    };
-
-    person.greet();
+    let mut person = Person::new(
+        String::from("foo"),
+        "bar".to_string(),
+        30
+    );
+    
+    let empty_person = Person::new_empty();
+    
+    Person::say_hello();
 
     println!("\nOld values");
     print!("{}\t{}\t{}\n", person.name, person.last_name, person.age);
+
+    person.greet();
     person.happy_birthday();
 
     let name = person.name;
@@ -43,7 +61,8 @@ fn main() {
 
     println!("\nNew values");
     print!("{name}\t{last_name}\t{age}\n");
-    
-    Person::say_hello();
+
+    println!("\nFrom empty constructor");
+    print!("{}\t{}\t{}\n", empty_person.name, empty_person.last_name, empty_person.age);
 }
  
