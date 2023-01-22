d2c54c928cf0e79149745a9e75ea4c429db5402e
Eduardo Galeano <cegard689@gmail.com>
2023-01-22 23:00:24 +0100

    Using modules.

--- main.rs
+++ main.rs
@@ -5,14 +5,14 @@
 
 // use snake_game_v1::* imports everything which is public
 
-use snake_game_v1::{Person, Animal, display, display_the_2nd};
+use snake_game_v1::learn_rust::{Person, Animal, display, display_the_2nd};
 
 fn main() {
     let person = Person::from(
         String::from("foo"),
         "bar".to_string(),
         30,
-        snake_game_v1::PersonID::Passpport("XYZ".to_string(), 112233)
+        snake_game_v1::learn_rust::PersonID::Passpport("XYZ".to_string(), 112233)
     );
 
     let animal = Animal("Dog".to_string(), 4, "Woof!".to_string());
