c7218a2969421817f2d40dbde7fabe65757f5227
Eduardo Galeano <cegard689@gmail.com>
2023-01-22 22:55:57 +0100

    Different imports.

--- main.rs
+++ main.rs
@@ -1,14 +1,18 @@
-use snake_game::Person;
-use snake_game::Animal;
-use snake_game::display;
-use snake_game::display_the_2nd;
+// use snake_game_v1::Person;
+// use snake_game_v1::Animal;
+// use snake_game_v1::display;
+// use snake_game_v1::display_the_2nd;
+
+// use snake_game_v1::* imports everything which is public
+
+use snake_game_v1::{Person, Animal, display, display_the_2nd};
 
 fn main() {
     let person = Person::from(
         String::from("foo"),
         "bar".to_string(),
         30,
-        snake_game::PersonID::Passpport("XYZ".to_string(), 112233)
+        snake_game_v1::PersonID::Passpport("XYZ".to_string(), 112233)
     );
 
     let animal = Animal("Dog".to_string(), 4, "Woof!".to_string());
