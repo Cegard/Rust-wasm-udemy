6f55e19ab8240c4cdf661cc9f47d0f48e1b91334
Eduardo Galeano <cegard689@gmail.com>
2023-02-11 20:37:17 +0100

    Importig objects in .wasm.

--- www/public/index.ts
+++ www/public/index.ts
@@ -1,8 +1,18 @@
 
 async function init() {
+  const add_console = {
+    console: {
+      log: () => {
+      console.log("Logging from the .wasm file.");
+      },
+      error: () => {
+        console.log("Oops... That's all.");
+      }
+    }
+  };
   const response = await fetch("static/add.wasm");
   const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
+  const wasm = await WebAssembly.instantiate(buffer, add_console);
   const add = wasm.instance.exports.add;
 
   console.log(add(110, 89));
