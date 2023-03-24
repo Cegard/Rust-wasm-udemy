054ec8229331e7b4d5d8e80cf1916cce9acaad5f
Eduardo Galeano <cegard689@gmail.com>
2023-03-23 21:00:46 -0500

    Adding compiled WASM to public files.

--- www/public/index.ts
+++ www/public/index.ts
@@ -1,29 +1,5 @@
+import init, { greet } from 'snake_game_v1';
 
-async function init() {
-  const memory = new WebAssembly.Memory({initial: 1});
-
-  const add_console = {
-    js: {
-      mem: memory
-    },
-    console: {
-      log: () => {
-      console.log("Logging from the .wasm file.");
-      },
-      error: () => {
-        console.log("Oops... That's all.");
-      }
-    }
-  };
-
-  const response = await fetch("static/add.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer, add_console);
-  const add = wasm.instance.exports.add;
-  const uInt8Array = new Uint8Array(memory.buffer, 0, 3);
-  const decodedMessage = new TextDecoder().decode(uInt8Array);
-
-  console.log(add(110, 89), decodedMessage);
-}
-
-init();
+init().then(_ => {
+  greet("Eddie");
+});
