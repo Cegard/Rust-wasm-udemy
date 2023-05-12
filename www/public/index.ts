72a885316628f158afa7ca7f5e0d0200cb26ea1d
Eduardo Galeano <cegard689@gmail.com>
2023-05-12 00:31:54 -0500

    REfactor on src/index.ts performed.

--- www/public/index.ts
+++ www/public/index.ts
@@ -1,38 +1,46 @@
 import init, {World} from 'snake_game_v1';
 import {setDrawer} from '../src/drawers';
 
-const TIMEOUT = 200;
-const CELL_SIZE = 40;
+function update(
+  width: number,
+  height: number,
+  context: CanvasRenderingContext2D,
+  world: World,
+  draw: () => void
+) {
+  const TIMEOUT = 100;
+
+  function updateDelayed() {
+    setTimeout(() => {
+      context.clearRect(0, 0, width, height);
+      world.update();
+      draw();
+      requestAnimationFrame(updateDelayed); // the callback will be invoked before the next browser re-paint
+    }, TIMEOUT);
+  }
+  
+  updateDelayed();
+}
 
 async function start() {
+  const CELL_SIZE = 35;
 
   await init();
 
   const world = World.new();
+  
   const canvas = <HTMLCanvasElement> document.getElementById("game-canvas");
-  if (canvas === null) return
+  if (canvas === null) return;
+  
+  canvas.height = canvas.width = world.size() * CELL_SIZE;
 
   const context = canvas.getContext("2d");
-  if (context === null) return
-
-  context.canvas.width = world.size() * CELL_SIZE;
-  context.canvas.height = world.size() * CELL_SIZE;
-
-  const width = canvas.width;
-  const height = canvas.height;
+  if (context === null) return;
+  
   const draw = setDrawer(context, world, CELL_SIZE);
+
   draw();
-
-  function update(context: CanvasRenderingContext2D, canvas: HTMLCanvasElement, world: World, ) {
-    setTimeout(() => {
-    context.clearRect(0, 0, width, height);
-      world.update();
-      draw();
-      requestAnimationFrame(() => update(context, canvas, world)); // the callback will be invoked before the nezt browser re-paint
-   }, TIMEOUT);
-  }
-
-  update(context, canvas, world);
+  update(canvas.height, canvas.width, context, world, draw);
 }
 
 start();
