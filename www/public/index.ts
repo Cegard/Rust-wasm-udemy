75ee6edc8a92c776945196c1efaf6d6d2fcd26db
Eduardo Galeano <cegard689@gmail.com>
2023-05-24 19:32:13 -0500

    Eslinted.

--- www/public/index.ts
+++ www/public/index.ts
@@ -1,14 +1,14 @@
-import init, { World, Direction } from 'snake_game_v1';
-import { setDrawer } from '../src/drawers';
-import { randomInt } from '../src/helpers';
-import { DirectionsType } from '../src/types';
+import init, { World, Direction } from "snake_game_v1";
+import { setDrawer } from "../src/drawers";
+import { randomInt } from "../src/helpers";
+import { DirectionsType } from "../src/types";
 
 const directions: DirectionsType = {
   ArrowUp: Direction.Up,
   ArrowRight: Direction.Right,
   ArrowDown: Direction.Down,
   ArrowLeft: Direction.Left,
-}
+};
 
 function update(
   width: number,
@@ -27,26 +27,22 @@ function update(
       requestAnimationFrame(updateDelayed); // the callback will be invoked before the next browser re-paint
     }, 1000 / SPEED);
   }
-  
+
   updateDelayed();
 }
 
-function build_snake(
-  buffer: ArrayBuffer,
-  world: World
-): Uint32Array {
-
+function build_snake(buffer: ArrayBuffer, world: World): Uint32Array {
   return new Uint32Array(
     buffer,
     world.get_snake_cells(),
     world.get_snake_length()
-  )
+  );
 }
 
 async function start() {
   const CELL_SIZE = 35;
   const WORLD_WIDTH = 8;
-  const SNAKE_LENGTH = 4
+  const SNAKE_LENGTH = 4;
 
   let direction = Direction.Right;
   const snakeSpawnIdx = randomInt(0, Math.pow(WORLD_WIDTH, 2));
@@ -54,21 +50,26 @@ async function start() {
   const wasm = await init();
 
   const world = World.new(WORLD_WIDTH, snakeSpawnIdx, direction, SNAKE_LENGTH);
-  
-  const canvas = <HTMLCanvasElement> document.getElementById('game-canvas');
+
+  const canvas = <HTMLCanvasElement>document.getElementById("game-canvas");
   if (canvas === null) return;
-  
+
   canvas.height = canvas.width = world.size() * CELL_SIZE;
 
-  const context = canvas.getContext('2d');
+  const context = canvas.getContext("2d");
   if (context === null) return;
 
-  const draw = setDrawer(context, world.size(), CELL_SIZE, build_snake(wasm.memory.buffer, world));
+  const draw = setDrawer(
+    context,
+    world.size(),
+    CELL_SIZE,
+    build_snake(wasm.memory.buffer, world)
+  );
 
   draw();
   update(canvas.height, canvas.width, context, world, draw);
 
-  document.addEventListener('keydown', e => {
+  document.addEventListener("keydown", (e) => {
     direction = directions[e.code] ?? direction;
     world.change_snake_direction(direction);
   });
