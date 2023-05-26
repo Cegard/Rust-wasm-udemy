a5b58146bdc83adac1734513f1d464901139fda4
Eduardo Galeano <cegard689@gmail.com>
2023-05-26 00:09:59 -0500

    88. Move cells.

--- src/lib.rs
+++ src/lib.rs
@@ -33,19 +33,6 @@ impl Snake {
             direction
         };
     }
-
-    fn change_direction(&mut self, direction: Direction) {
-        self.direction = direction;
-    }
-
-    fn get_head_idx(&self) -> usize {
-        
-        return self.body[0].0;
-    }
-
-    fn set_head(&mut self, head: SnakeCell) {
-        self.body[0] = head;
-    }
 }
 
 #[wasm_bindgen]
@@ -65,11 +52,11 @@ impl World {
 
     pub fn get_snake_head(&self) -> usize {
 
-        return self.snake.get_head_idx();
+        return self.snake.body[0].0;
     }
 
     pub fn change_snake_direction(&mut self, direction: Direction) {
-        self.snake.change_direction(direction);
+        self.snake.direction = direction;
     }
 
     pub fn get_snake_length(&self) -> usize {
@@ -86,27 +73,40 @@ impl World {
 
     pub fn step(&mut self) {
         let last_idx = self.length.pow(2);
-        let snake_head_idx = self.snake.get_head_idx() as isize;
+        let snake_head_idx = self.snake.body[0].0 as isize;
         let vert_thresholds = [-1, last_idx];
         let hor_thresholds = [
             snake_head_idx/self.length * self.length - 1,
             snake_head_idx/self.length * self.length + self.length
         ];
 
-        self.snake.set_head(SnakeCell(match self.snake.direction {
+        self.snake.body[0].0 = match self.snake.direction {
             Direction::Up => if (snake_head_idx - self.length) > vert_thresholds[0]
-                             { (snake_head_idx - self.length) as usize }
-                             else { (last_idx - self.length + snake_head_idx) as usize }
+                            { (snake_head_idx - self.length) as usize }
+                            else { (last_idx - self.length + snake_head_idx) as usize }
             Direction::Right => if (snake_head_idx + 1) < hor_thresholds[1]
-                                { self.snake.get_head_idx() + 1 }
+                                { self.snake.body[0].0 + 1 }
                                 else { (snake_head_idx + 1 - self.length) as usize }
             Direction::Down => if (snake_head_idx + self.length) < vert_thresholds[1]
-                               { (snake_head_idx + self.length) as usize }
-                               else { (snake_head_idx + self.length - last_idx) as usize }
+                            { (snake_head_idx + self.length) as usize }
+                            else { (snake_head_idx + self.length - last_idx) as usize }
             Direction::Left => if (snake_head_idx - 1) > hor_thresholds[0]
-                               { self.snake.get_head_idx() - 1 }
-                               else { (snake_head_idx + self.length - 1) as usize }
-        }));
+                            { self.snake.body[0].0 - 1 }
+                            else { (snake_head_idx + self.length - 1) as usize }
+        };
+
+        self.move_snake_body(snake_head_idx as usize);
+    }
+
+    fn move_snake_body(&mut self, prev_head_idx: usize) {
+        let snake_length = self.snake.body.len();
+        let mut prev_idx = prev_head_idx;
+
+        for i in 1..snake_length {
+            let temp_idx = self.snake.body[i].0;
+            self.snake.body[i].0 = prev_idx;
+            prev_idx = temp_idx;
+        }
     }
 }
 
